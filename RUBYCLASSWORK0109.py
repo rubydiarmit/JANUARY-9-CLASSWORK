184f84b32ee505aea50279e767ba3af9e3ff4b60
rubydiarmit <34691001+rubydiarmit@users.noreply.github.com>
2018-01-09 14:41:39 -0500

    Add files via upload

---
+++ RUBYCLASSWORK0109.py
@@ -0,0 +1,11 @@
+print("Welcome to GMail")
+myUsername= input("Enter your username, then press return: ")
+
+if myUsername== "johndoe":
+    print("Username Successful")
+    
+myPassword=input("Enter your password, then press enter: ")
+if myPassword== "sof123@":
+    print("Success")
+else:
+    print("Unable to login. Incorrect Password")
